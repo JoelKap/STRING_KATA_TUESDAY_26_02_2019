e0bbe63397206383bbea616486cb3eb10d018602
Joel Kapuku Ntumba <joelkapuku@hotmail.com>
2019-02-26 23:40:57 +0200

    #chore: complete STRING KATA TDD

---
+++ STRING_KATA_TUESDAY_26_02_2019/NegativeNotSupported.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace STRING_KATA_TUESDAY_26_02_2019
+{
+    [Serializable]
+    internal class NegativeNotSupported : Exception
+    {
+        public NegativeNotSupported()
+        {
+        }
+
+        public NegativeNotSupported(string message) : base(message)
+        {
+        }
+
+        public NegativeNotSupported(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NegativeNotSupported(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
